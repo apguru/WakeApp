0da12bffafdd91199b0e792023560317dfd29d07
Alexander Piotrowski <spielexd@hotmail.de>
2022-05-12 13:02:49 +0200

    Initial Reformat and UI

--- src/main/java/com/example/wakeapp/WakeApp.java
+++ src/main/java/com/example/wakeapp/WakeApp.java
@@ -3,16 +3,19 @@ package com.example.wakeapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class WakeApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(WakeApp.class.getResource("indexView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 800);
+        stage.setTitle("WakeApp");
         stage.setScene(scene);
         stage.show();
     }
